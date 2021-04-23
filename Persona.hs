c1b32a3c63f99058dbfbf5ba40a6be2865130944
Lisandro Iraguen <lisandro.iraguen@gmail.com>
2021-04-23 19:01:42 -0300

    definicion de persona

--- Persona.hs
+++ Persona.hs
@@ -1,1 +1,18 @@
 module Persona where
+
+data Persona = UnaPersona { nombre :: String  
+                     , edad :: Int  
+                     , suenios :: String  
+                     , losFelicidonios :: String
+                     , hablidad :: String
+                     } deriving (Show)
+
+--genero los estudiantes son los datos
+pepito::Persona
+pepito = UnaPersona {
+   nombre= "pepito",
+   edad = 2,
+   suenios = "1000000/2",
+   losFelicidonios = "01/01/2010",
+   hablidad="ninguna x ahora"
+}  
